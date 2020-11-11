54d3dbc465b0b5fbf5c1344d3d39d4b3cbaf5708
hiroyo <hiroyo@nibb.ac.jp>
2020-11-11 15:42:06 +0900

    Add 1st stanza

---
+++ stanzas/ortholog_group_descriptions/README.md
@@ -0,0 +1,3 @@
+# Ortholog group descriptions
+
+Stanza description goes here. Edit `stanzas/ortholog_group_descriptions/README.md` to update.
---
+++ stanzas/ortholog_group_descriptions/index.js
@@ -0,0 +1,8 @@
+export default async function orthologGroupDescriptions(stanza, params) {
+  stanza.render({
+    template: 'stanza.html.hbs',
+    parameters: {
+      greeting: `Hello, ${params['say-to']}!`
+    }
+  });
+}
